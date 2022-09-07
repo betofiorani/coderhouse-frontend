c57ba5825f8c50a21d56aea6ebaf1619665ec943
Beto Fiorani <betofiorani@gmail.com>
2022-09-06 21:09:12 -0300

    second commit

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,34 +1,41 @@
 import axios from 'axios'
 
+const baseUrl = process.env.REACT_APP_BACKEND_URL;
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, PUT, PATCH, POST, DELETE, OPTIONS',
+};
+
 const getProducts = async () => {
 
-  const {data} = await axios.get('http://localhost:8080/api/productos')
+  const {data} = await axios.get(`${baseUrl}/api/productos`,{headers})
 
   return data
 }
 
 const getProductById = async (id) => {
 
-  const {data} = await axios.get(`http://localhost:8080/api/productos/${id}`)
+  const {data} = await axios.get(`${baseUrl}/api/productos/${id}`,{headers})
 
   return data
 }
 
 
 const createProduct = async (productData) => {
-  const {data} = await axios.post(`http://localhost:8080/api/productos`,productData)
+  const {data} = await axios.post(`${baseUrl}/api/productos`,productData,{headers})
   
   return data
 }
 
 const updateProduct = async (productData) => {
-  const {data} = await axios.put(`http://localhost:8080/api/productos/${productData.id}`,productData)
+  const {data} = await axios.put(`${baseUrl}/api/productos/${productData.id}`,productData,{headers})
   
   return data
 }
 
 const deleteProduct = async productId => {
-  const {data} = await axios.delete(`http://localhost:8080/api/productos/${productId}`)
+  const {data} = await axios.delete(`${baseUrl}/api/productos/${productId}`,{headers})
 
   return data
 }
